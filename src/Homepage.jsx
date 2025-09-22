c6c7281082dd1bbb64a01450a5a2668b54c6b7a4
Tanvi <Tanvi@LAPTOP-U3SV057G>
2025-09-22 17:56:40 +0530

    image

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -11,6 +11,7 @@ const SkillIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" fill="none" vi
 const AptitudeIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 1.085-1.085-1.085m0 0V14.25h2.17m-2.17 0H12m0 0v2.25m0 0h1.5m-1.5 0H9.75m4.5 0H12m0 0V3.75m0 0h-1.5m1.5 0H12m0 0h1.5m-1.5 0H9.75" /></svg> );
 const VerbalIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" /></svg> );
 const AffairsIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" /></svg> );
+const ComputerIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M96 176c0-79.5 64.5-144 144-144 55.4 0 103.5 31.3 127.6 77.1 14.2-8.3 30.8-13.1 48.4-13.1 53 0 96 43 96 96 0 15.7-3.8 30.6-10.5 43.7 44 20.3 74.5 64.7 74.5 116.3 0 70.7-57.3 128-128 128l-304 0C64.5 480 0 415.5 0 336 0 272.6 41 218.8 97.9 199.5 96.7 191.9 96 184 96 176zM240 48c-70.7 0-128 57.3-128 128 0 9.4 1 18.6 2.9 27.4 .9 4.1-1.6 8.3-5.7 9.4-53.8 15.1-93.3 64.6-93.3 123.2 0 70.7 57.3 128 128 128l304 0c61.9 0 112-50.1 112-112 0-48-30.2-89-72.8-104.9-2.3-.8-4-2.7-4.8-5s-.4-4.8 .9-6.8c8-12.5 12.7-27.3 12.7-43.2 0-44.2-35.8-80-80-80-17.6 0-33.8 5.7-47 15.2-2 1.4-4.4 1.9-6.8 1.3s-4.3-2.3-5.2-4.5c-19.9-44.8-64.8-76-117-76z"/></svg>);
 const ProfileIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" /></svg> );
 
 function Logo() {
@@ -98,7 +99,7 @@ function Homepage() {
                 <li><a className="sidebar-link"><AptitudeIcon /><span className="link-text">General Aptitude</span></a></li>
                 <li><a className="sidebar-link"><VerbalIcon /><span className="link-text">Verbal and Reasoning</span></a></li>
                 <li><a className="sidebar-link"><AffairsIcon /><span className="link-text">Current Affairs</span></a></li>
-                <li><a className="sidebar-link"><SkillIcon /><span className="link-text">Computer Science</span></a></li>
+                <li><a className="sidebar-link"><ComputerIcon /><span className="link-text">Computer Science</span></a></li>
                 <li><a className="sidebar-link"><AptitudeIcon /><span className="link-text">Engineering</span></a></li>
             </ul>
           </div>
